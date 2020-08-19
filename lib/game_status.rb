a71877023801c6a84d58eea37054330bae32f4b5
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-08-19 01:58:54 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,8 +28,6 @@ def won?(board)
 
     if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
       return win_combination
-    else
-      false
     end
   end
   return false
